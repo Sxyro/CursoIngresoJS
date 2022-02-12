5009124570c52c34922b7ee8dfe9127fe913d1b5
Sxyro <bumatiasxy@gmail.com>
2022-02-12 18:49:14 -0300

    tp 3

--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -4,10 +4,21 @@ B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en
 */
 function FahrenheitCentigrados () 
 {
-	
+	let fahrenheit = parseInt(document.getElementById("txtIdTemperatura").value);
+	let centrigados = (fahrenheit - 32) * (5/9);
+	centrigados = centrigados.toFixed(2);
+	alert(`${fahrenheit} grados Fahrenheit son ${centrigados} grados Centigrados`)
 }
 
 function CentigradosFahrenheit () 
 {
-	
+	let centigrados = parseInt(document.getElementById("txtIdTemperatura").value);
+	let fahrenheit = (centigrados * 1.8) + 32;
+	fahrenheit = fahrenheit.toFixed(2);
+	alert(`${centigrados} grados Centigrados son ${fahrenheit} grados Fahrenheit`);
 }
+
+
+// un grado Fahrenheit es (X °C * 1.8) + 32
+
+// un grado Celsius es (X °F - 32) * (5/9)
