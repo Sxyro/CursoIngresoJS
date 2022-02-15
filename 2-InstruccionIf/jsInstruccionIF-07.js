97fd11181a790f7a12c52aa4cffafcbe25de6514
Sxyro <bumatiasxy@gmail.com>
2022-02-15 18:45:55 -0300

    ejercicio 7 if

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,8 +1,14 @@
 function mostrar()
 {
 	//tomo la edad  
-	alert("ok");
-	
+let edad = document.getElementById("txtIdEdad").value;
+let estadoCivil = document.getElementById("estadoCivil").value;
 
+if (edad <= 17 && estadoCivil != "Soltero") {
+	alert("Sos muy joven para no ser soltero.");
+}
+else {
+	alert("xd");
+}
 
 }//FIN DE LA FUNCIÓN
