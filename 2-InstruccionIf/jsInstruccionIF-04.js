27096a297099252779fa62a76a14620ba1ed894f
Sxyro <bumatiasxy@gmail.com>
2022-02-15 18:45:36 -0300

    ejercicio 4 if

--- 2-InstruccionIf/jsInstruccionIF-04.js
+++ 2-InstruccionIf/jsInstruccionIF-04.js
@@ -1,7 +1,12 @@
 function mostrar()
 {
 	//tomo la edad  
-	alert("ok");
+let edad = document.getElementById("txtIdEdad").value;
 
+if (edad >= 13 && edad <= 17) {
 
+	alert("Sos adolescente");
+}
+
+alert("Fin de la función");
 }//FIN DE LA FUNCIÓN
