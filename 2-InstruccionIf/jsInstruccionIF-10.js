e9aecfa2c180cd12755d6ffa8eeb4bc4d8f336e3
Sxyro <bumatiasxy@gmail.com>
2022-02-15 18:46:22 -0300

    ejercicio 10 if

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,14 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
-
+	let nota = Math.round(Math.random() * 9 + 1);
+	if (nota >= 9) {
+		alert("Excelente.")
+	}	
+	else if (nota <= 8 && nota >= 5) {
+		alert("Aprobó");
+	}
+	else {
+		alert("No pasa nada capo, la próxima será.")
+	}
 }//FIN DE LA FUNCIÓN
