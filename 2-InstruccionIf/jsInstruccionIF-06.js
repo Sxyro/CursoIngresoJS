cf9a1833db5b4f91c4cde5a5735c746f3fb18129
Sxyro <bumatiasxy@gmail.com>
2022-02-15 18:45:49 -0300

    ejercicio 6 if

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,8 +1,16 @@
 function mostrar()
 {
 	//tomo la edad  
-	alert("ok");
+let edad = document.getElementById("txtIdEdad").value;
 
+if (edad <= 13) {
 
-
+	alert("Sos un niño");
+}
+else if (edad >= 13 && edad <= 17) {
+	alert ("Sos adolescente");
+}
+else {
+	alert("Sos mayor de edad");
+}
 }//FIN DE LA FUNCIÓN
