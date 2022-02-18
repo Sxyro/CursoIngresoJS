084dcd392ce42f5429478d0ccb18791888363293
Sxyro <bumatiasxy@gmail.com>
2022-02-18 16:35:49 -0300

    ejercicio 1 while

--- 5-InstruccionWhile/jsIteracionesWhile-01.js
+++ 5-InstruccionWhile/jsIteracionesWhile-01.js
@@ -3,5 +3,11 @@ al presionar el botón mostrar 10 repeticiones
 con números ASCENDENTE, desde el 1 al 10.*/
 function mostrar()
 {
-	alert('iteración while');
-}//FIN DE LA FUNCIÓN
+	let contador = 0; // 1. Primera parte de la iteración.
+	while (contador < 10) // 2. Segunda parte de la iteración.
+	{
+		contador = contador +1; // 3. Tercera parte de la iteración.
+		alert(contador);
+	}
+}
+	// alert('iteración while');
