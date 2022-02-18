55009bcc3a4eb3160016cdde7f1dee829cfd73a7
Sxyro <bumatiasxy@gmail.com>
2022-02-18 16:36:19 -0300

    ejercicio 6 while

--- 5-InstruccionWhile/jsIteracionesWhile-06.js
+++ 5-InstruccionWhile/jsIteracionesWhile-06.js
@@ -1,13 +1,19 @@
 function mostrar()
 {
-	var contador;
-	var acumulador;
-	var numeroIngresado;
+	let contador;
+	let acumulador;
+	let numeroIngresado;
 
-	contador=0;
-	acumulador=0;
+	contador = 0;
+	acumulador = 0;
 	
-	
-	txtIdSuma.value=acumulador;
-	txtIdPromedio.value=acumulador/5;
+	while (contador < 3)
+	{
+		numeroIngresado = prompt("Ingrese los datos");
+		numeroIngresado = parseInt(numeroIngresado);
+		acumulador = acumulador + numeroIngresado;
+		contador = contador +1
+	}
+	document.getElementById("txtIdSuma").value= acumulador;
+	document.getElementById("txtIdPromedio").value=acumulador / contador;
 }//FIN DE LA FUNCIÓN
