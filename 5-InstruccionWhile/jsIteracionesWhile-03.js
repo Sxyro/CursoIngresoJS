88086fa9a954b1a2962425a87c4501213a182fce
Sxyro <bumatiasxy@gmail.com>
2022-02-18 16:36:02 -0300

    ejercicio 3 while

--- 5-InstruccionWhile/jsIteracionesWhile-03.js
+++ 5-InstruccionWhile/jsIteracionesWhile-03.js
@@ -2,7 +2,28 @@
 al presionar el botón pedir la CLAVE (ayuda: es utn750)*/
 function mostrar()
 {
-	var claveIngresada;
+	let claveIngresada;
 	claveIngresada = prompt("ingrese el número clave.");
-	
+
+	while (claveIngresada != "utn750")
+	{
+		claveIngresada = prompt("La clave es incorrecta, Ingrese el número clave nuevamente.");
+	}
+	alert("Bienvenido!");
+
+	// if (claveIngresada != "utn750") 
+	// {
+	// 	claveIngresada = prompt("La clave es incorrecta, Ingrese el número clave nuevamente.");
+	// 	if (claveIngresada != "utn750") {
+	// 		claveIngresada = prompt("La clave es incorrecta, Ingrese el número clave nuevamente.");
+	// 	}
+	// 	else
+	// 	{
+
+	// 	}
+	// }
+	// else 
+	// {
+	// 	alert("Bienvenido");
+	// }
 }//FIN DE LA FUNCIÓN
