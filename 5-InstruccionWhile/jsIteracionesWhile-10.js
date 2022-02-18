d9849cefacae5ad7566e0bd27fa3287ab267a5a4
Sxyro <bumatiasxy@gmail.com>
2022-02-18 16:36:45 -0300

    ejercicio 10 while

--- 5-InstruccionWhile/jsIteracionesWhile-10.js
+++ 5-InstruccionWhile/jsIteracionesWhile-10.js
@@ -1,5 +1,4 @@
-/*
-Al presionar el botón pedir  números  
+/* Al presionar el botón pedir  números  
 hasta que el usuario quiera, mostrar:
 1-Suma de los negativos.
 2-Suma de los positivos.
@@ -12,18 +11,52 @@ hasta que el usuario quiera, mostrar:
 9-Diferencia entre positivos y negativos, (positvos-negativos). */
 function mostrar()
 {
-	//declarar contadores y variables 
-	var respuesta;
-	var numeroIngresado;
-	var sumaNegativos=0;
+	let numeroIngresado;
+	let sumaPositivos = 0;
+	let sumaNegativos = 0;
+	let cantidadPositivos = 0;
+	let cantidadNegativos = 0;
+	let sumaCeros = 0;
+	let cantidadNumerosPares = 0;
+	let promedioPositivos = 1;
+	let promedioNegativos = 1;
+	let diferencia;
+	let respuesta = "si";
 
-	respuesta="si";
-
-	while(respuesta=="si")
-	{
+	do{
 		
-		respuesta=prompt("desea continuar?");
-	}//fin del while
+		numeroIngresado = parseInt(prompt("Ingrese un número: ")); // Solicito un numero
+		
+		if (numeroIngresado == 0){
+			sumaCeros++;
+		}
+		else if (numeroIngresado > 0) {
+			cantidadPositivos++;
+			sumaPositivos = sumaPositivos + numeroIngresado;
+		}
+		else {
+			cantidadNegativos++;
+			sumaNegativos = sumaNegativos + numeroIngresado;
+		}
 
-	document.write("la suma de negativos es :"+sumaNegativos);
-}//FIN DE LA FUNCIÓN
+		if (numeroIngresado % 2 == 0) {
+			cantidadNumerosPares++;
+		}
+
+		respuesta = prompt("Para ingresar otro número escriba 'si'");
+	}while (respuesta == 'si');
+
+	promedioPositivos = sumaPositivos / cantidadPositivos;
+	promedioNegativos = sumaNegativos / cantidadNegativos;
+	diferencia = sumaPositivos - sumaNegativos;
+
+	document.write(`1. La suma de positivos es: ${sumaPositivos} <br>
+					2. La suma de negativos es: ${sumaNegativos} <br>
+					3. La cantidad de positivos es: ${cantidadPositivos} <br>
+					4. La cantidad de negativos es: ${cantidadNegativos} <br>
+					5. La cantidad de ceros es: ${sumaCeros} <br>
+					6. La cantidad de numeros pares es: ${cantidadNumerosPares} <br>
+					7. El promedio de los números positivos es: ${promedioPositivos} <br>
+					8. El promedio de los números negativos es: ${promedioNegativos} <br>
+					9. La diferencia entre positivos y negativos es: ${diferencia}`);
+}
