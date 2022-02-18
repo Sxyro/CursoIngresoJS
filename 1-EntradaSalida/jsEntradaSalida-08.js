e03a9affaf5a51b14771a97b99576ce209a11fce
Sxyro <bumatiasxy@gmail.com>
2022-02-18 16:34:42 -0300

    ejercicio 8

--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -1,5 +1,4 @@
-/*
-Debemos lograr tomar Los numeros por ID ,
+/* Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
