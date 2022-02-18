e17745b668f9819850ba0cad77212c965d6db54f
Sxyro <bumatiasxy@gmail.com>
2022-02-18 16:35:17 -0300

    ejercicio 4 switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,11 +1,19 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-	
-	
+	let mes = document.getElementById("txtIdMes").value;
 
-
-
-}//FIN DE LA FUNCIÓN
+	switch (mes) {
+		case "Febrero":
+			alert("Este mes tiene 29 días")
+		break;
+		case "Abril":
+		case "Junio":
+		case "Septiembre":
+		case "Noviembre":
+			alert("Este mes tiene 30 días");
+		break;
+		default: 
+			alert("Este mes tiene 31 días");
+		break;
+	}
+}
