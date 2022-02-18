c5d9a2b4170d6f4fe61e7dcca5f03fa198ddd6b9
Sxyro <bumatiasxy@gmail.com>
2022-02-18 16:36:06 -0300

    ejercicio 4 while

--- 5-InstruccionWhile/jsIteracionesWhile-04.js
+++ 5-InstruccionWhile/jsIteracionesWhile-04.js
@@ -3,7 +3,12 @@ al presionar el botón
 pedir un número entre 0 y 9 inclusive.*/
 function mostrar()
 {
-	var numeroIngresado;
-	numeroIngresado = prompt("ingrese un número entre 0 y 10.");
-	
+	let numeroIngresado = prompt("Ingrese un número entre 0 y 9.");
+
+	while (numeroIngresado <= 0 || numeroIngresado >= 10) 
+	{
+		numeroIngresado = prompt("Ese número está fuera del rango solicitado. Ingrese nuevamente");
+	}
+	document.getElementById("txtIdNumero").value = numeroIngresado;
+
 }//FIN DE LA FUNCIÓN
